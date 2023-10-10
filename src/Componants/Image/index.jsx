0de8f62c3f1c3ceab398c191c0d081a0e5c04af9
Pepito Mercurio <coranthin.etheve@ynov.com>
2023-10-10 12:52:03 +0200

    testHebergement

--- src/Componants/Image/index.jsx
+++ src/Componants/Image/index.jsx
@@ -6,7 +6,7 @@ import "../../Style/scss/Image/style.scss"
 export default function Image() {
     return (
         <div className="Image">
-            <img src="https://images.cnrs.fr/system/files/styles/full_width_mobile/private/media/images/2011/07/CNRS_20110001_1937_683.jpg?h=b8e51141&itok=D7vgv8W2"/>
+            <img src="https://images.cnrs.fr/system/files/styles/full_width_mobile/private/media/images/2011/07/CNRS_20110001_1937_683.jpg?h=b8e51141&itok=D7vgv8W2" alt="test"/>
             <div className="icons">
                 <FontAwesomeIcon className="Card-download" icon={faDownload} />
                 <FontAwesomeIcon className="Card-Like" icon={faHeart} />
