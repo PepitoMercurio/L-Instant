1932b6977a11863476aeece566e052bce22e1e8d
Pepito Mercurio <coranthin.etheve@ynov.com>
2023-10-10 12:54:50 +0200

    testHebergement

--- src/Componants/Footer/index.jsx
+++ src/Componants/Footer/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import "../../Style/scss/Footer/style.scss";
 
 export default function Footer(props) {
